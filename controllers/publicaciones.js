19875783085c37d7b58cdc37d08ef1f1ceb15741
isaxz <71412802+isaxz@users.noreply.github.com>
2023-06-12 14:22:59 -0600

    habilitar funcion "create"

--- controllers/publicaciones.js
+++ controllers/publicaciones.js
@@ -17,18 +17,18 @@ class PostsController {
         }*/
     }
 
-    /*
+    
     async create(req, res, next) {
         console.log(req.method);
         if (req.method === 'POST') {
             await Post.create({
-                nombre: req.body.nombre,
-                apellidos: req.body.apellidos, 
-                telefonoCasa: req.body.telefonoCasa, 
-                dirCasa: req.body.dirCasa, 
-                telefonoTrabajo: req.body.telefonoTrabajo, 
-                dirTrabajo: req.body.dirTrabajo, 
-                email: req.body.email
+                title: req.body.title,
+                date: req.body.date, 
+                image: req.body.image, 
+                text: req.body.text, 
+                author: req.body.author, 
+                category: req.body.category, 
+                numComments: req.body.numComments
             });
             res.redirect('/publicacion');
         }
@@ -36,7 +36,7 @@ class PostsController {
             res.render('publicacion/create', { title: 'Base de Datos de Direcciones, crear'});
         }
     }
-
+    /*
     async update(req, res, next) {
         if (req.method === 'POST') {
             await Post.update(
